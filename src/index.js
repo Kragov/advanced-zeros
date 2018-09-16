5be887cb2a96c97893d659d949bdae6ae3101cfd
Kragov <robrozdestvensky@gmail.com>
2018-09-16 23:35:45 +0300

    why not 100 ? I dunno

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function getZerosCount(number, base) {
+module.exports = function getZerosCount(number, base) {
   let biggestSimpleNumber = 0;
   let baseCopy = base;
   let counter = 0;
@@ -6,22 +6,15 @@ function getZerosCount(number, base) {
         while (((baseCopy % i) === 0) && (base > 1)) {
             biggestSimpleNumber = i;
             baseCopy /= i;
-            console.log(baseCopy, biggestSimpleNumber)
         }
   }
 
   let multiplier = biggestSimpleNumber;
 
-  console.log(multiplier)
   while (biggestSimpleNumber < number) {
-      console.log(biggestSimpleNumber, counter)
       counter += Math.trunc(number / biggestSimpleNumber);
       biggestSimpleNumber *= multiplier;
   }
 
   return counter;
 }
-
-console.log(getZerosCount(82610032, 144))
-
-// module.exports =
